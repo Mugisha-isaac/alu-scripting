fa7a1b4630368161e0597cd308a8d8041f444c6a
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-03 10:05:31 +0200

    fix: handle 404

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,14 +12,19 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API and returns the number of subscribers
     for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "custom-script/1.0"}
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit=subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            return response.json().get("data", {}).get("subscribers", 0)
+            return response.json().get("data").get("subscribers")
+
     except requests.RequestException:
         return 0
+
     return 0
 
 
