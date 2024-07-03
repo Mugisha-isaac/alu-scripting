775228f8e369593ac09216243ec10747e3ea7777
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-03 14:26:30 +0200

    fix: 1-top response json

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -12,7 +12,6 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit=subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -15,18 +15,21 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{subreddit}/hot.json?limit=10".format(
         subreddit=subreddit
     )
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        for post in response.json().get("data").get("children"):
-            print(post.get("data").get("title"))
+        posts = response.json()["data"]["children"]
+        for post in posts:
+            print(post["data"]["title"])
     else:
         print("None")
+        return
 
 
 if __name__ == "__main__":
     subreddit = argv[1]
-    top_ten(subreddit)
+    if len(argv) < 2:
+        print("Please pass an argument")
+    else:
+        top_ten(subreddit)
